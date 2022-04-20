0522b6dc25cff73e33c7440c96788e138c3ac051
guilherssousa <guilherssousa@gmail.com>
2022-04-20 17:09:53 -0300

    font size

--- app/routes/$.jsx
+++ app/routes/$.jsx
@@ -18,7 +18,7 @@ export default function ArticlePage() {
         <main className="md:w-full mt-8 md:mt-0 md:max-w-3xl px-6">
           <h2 className="text-3xl font-bold">{article.title}</h2>
 
-          <div className="text-2xl mt-2">{article.description}</div>
+          <div className="text-xl mt-2">{article.description}</div>
 
           <div className="mt-4 text-gray-700 dark:text-gray-300">
             publicado em {new Date(article.updatedAt).toLocaleString("pt-BR")}{" "}
