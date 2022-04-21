51a470505a8677bffda7ccfd0b9c29c54e6900a6
guilherssousa <guilherssousa@gmail.com>
2022-04-21 20:32:52 -0300

    Update favicon and image

--- app/root.jsx
+++ app/root.jsx
@@ -13,22 +13,26 @@ export const meta = () => ({
   charset: "utf-8",
   title: "Blog do Guilherme Sousa",
   viewport: "width=device-width,initial-scale=1",
+  description: "Publicações sobre tecnologia, desenvolvimento e programação.",
   "og:locale": "pt_BR",
   "og:type": "website",
   "og:title": "Blog do Guilherme Sousa",
   "og:description":
     "Publicações sobre experiências, técnicas e truques de desenvolvimento por Guilherme Sousa.",
   "og:site_name": "Blog do Guilherme Sousa",
-  "og:image": "https://github.com/guilherssousa.png",
+  "og:image": "https://github.com/pic.jpg",
   "og:image:width": "383",
   "og:image:height": "383",
-  "og:image:secure_url": "https://github.com/guilherssousa.png",
-  "og:image:type": "image/png",
+  "og:image:secure_url": "https://github.com/pic.jpg",
+  "og:image:type": "image/jpg",
   "twitter:card": "summary",
   "twitter:title": "Blog do Guilherme Sousa",
   "twitter:site": "@krteazy",
   "twitter:creator": "@krteazy",
-  "twitter:image": "https://github.com/guilherssousa.png",
+  "twitter:url": "https://blog.guilherssousa.dev",
+  "twitter:description":
+    "Publicações sobre experiências, técnicas e truques de desenvolvimento por Guilherme Sousa.",
+  "twitter:image": "https://github.com/pic.jpg",
 });
 
 export const links = () => {
