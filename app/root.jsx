c5999d8892017ccdd510a114968522f7dddbc683
guilherssousa <guilherssousa@gmail.com>
2022-04-21 20:35:54 -0300

    change image url and description

--- app/root.jsx
+++ app/root.jsx
@@ -13,17 +13,18 @@ export const meta = () => ({
   charset: "utf-8",
   title: "Blog do Guilherme Sousa",
   viewport: "width=device-width,initial-scale=1",
-  description: "Publicações sobre tecnologia, desenvolvimento e programação.",
+  description:
+    "Publicações sobre experiências, ténicas e truques de desenvolvimento por Guilherme Sousa",
   "og:locale": "pt_BR",
   "og:type": "website",
   "og:title": "Blog do Guilherme Sousa",
   "og:description":
     "Publicações sobre experiências, técnicas e truques de desenvolvimento por Guilherme Sousa.",
   "og:site_name": "Blog do Guilherme Sousa",
-  "og:image": "https://github.com/pic.jpg",
+  "og:image": "https://blog.guilherssousa.dev/pic.jpg",
   "og:image:width": "383",
   "og:image:height": "383",
-  "og:image:secure_url": "https://github.com/pic.jpg",
+  "og:image:secure_url": "https://blog.guilherssousa.dev/pic.jpg",
   "og:image:type": "image/jpg",
   "twitter:card": "summary",
   "twitter:title": "Blog do Guilherme Sousa",
@@ -32,7 +33,7 @@ export const meta = () => ({
   "twitter:url": "https://blog.guilherssousa.dev",
   "twitter:description":
     "Publicações sobre experiências, técnicas e truques de desenvolvimento por Guilherme Sousa.",
-  "twitter:image": "https://github.com/pic.jpg",
+  "twitter:image": "https://blog.guilherssousa.dev/pic.jpg",
 });
 
 export const links = () => {
