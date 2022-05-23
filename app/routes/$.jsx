771ce8a38eeefc93fff7ca21b63372737fb4f4a3
guilherssousa <guilherssousa@gmail.com>
2022-05-23 11:22:48 -0300

    change cover image value

--- app/routes/$.jsx
+++ app/routes/$.jsx
@@ -8,15 +8,23 @@ import { useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ params }) => getArticle(params["*"]);
 
-export const meta = ({ data }) => ({
-  title: `${data.title} por Guilherme Sousa`,
-  description: data.description,
-  "article:author": "Guilherme Sousa",
-  "article:published_time": data.updatedAt,
-  "og:title": `${data.title} por Guilherme Sousa`,
-  "og:description": data.description,
-  "og:type": "article",
-});
+export const meta = ({ data }) => {
+  const coverImage = data.coverImage
+    ? data.coverImage
+    : "https://blog.guilherssousa.dev/pic.jpg";
+  return {
+    title: `"${data.title}" por Guilherme Sousa`,
+    description: data.description,
+    "article:author": "Guilherme Sousa",
+    "article:published_time": data.updatedAt,
+    "og:title": `"${data.title}" por Guilherme Sousa`,
+    "og:description": data.description,
+    "og:type": "article",
+    "og:image": coverImage,
+    "twitter:image": coverImage,
+    "og:image:secure_url": coverImage,
+  };
+};
 
 export default function ArticlePage() {
   const article = useLoaderData();
